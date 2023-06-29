272d96101c558ed9d7a0b65a170c530435f9eb3b
Maxwell Countryman Skewes <maxskewes@gmail.com>
2023-06-29 15:06:57 -0700

    about page ' to &apos; x 2

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -16,11 +16,11 @@ const About = ({ data }: AboutProps) => {
       <SectionHeader title={title} />
       <div className='pb-8'>
         <p className='mb-6'>
-          I'm a full-stack developer with experience in graphic design.
+          I&apos;m a full-stack developer with experience in graphic design.
           I&apos;ve spent the past several years contributing to the design and
           development of interactive websites and custom applications projected
           to serve over 100,000 users. Since graduating from University of
-          Oregon's Full-Stack program, I&apos;ve collaborated with UX designers,
+          Oregon&apos;s Full-Stack program, I&apos;ve collaborated with UX designers,
           product managers, and other front and back-end developers throughout
           all phases of projects, facilitating the creation intuitive, building
           modern apps and websites for companies across sectors including legal
